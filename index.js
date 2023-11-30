a888bd26d98885e219cbda068770690b680d64de
paulpollob <paulpollob71@gmail.com>
2023-11-30 06:16:28 +0600

    trial

--- index.js
+++ index.js
@@ -13,32 +13,32 @@ app.use(express.json());
 
 
 // Load the model
-const model = tf.loadModel('best_model.h5');
+// const model = tf.loadModel('best_model.h5');
 
 
 // Define the endpoint for receiving and processing images
-app.post('/predict', (req, res) => {
-    const imageData = req.body.image;
-    const imageTensor = tf.image.decodeJpeg(imageData);
-    const image = tf.reshape(imageTensor, [1, 28, 28, 3]);
+// app.post('/predict', (req, res) => {
+//     const imageData = req.body.image;
+//     const imageTensor = tf.image.decodeJpeg(imageData);
+//     const image = tf.reshape(imageTensor, [1, 28, 28, 3]);
   
-    // Normalize the image data
-    const imageNormalized = image.cast('float32') / 255.0;
+//     // Normalize the image data
+//     const imageNormalized = image.cast('float32') / 255.0;
   
-    // Make the prediction
-    const prediction = model.predict(imageNormalized);
+//     // Make the prediction
+//     const prediction = model.predict(imageNormalized);
   
-    // Get the predicted class and label
-    const predictedClass = tf.argmax(prediction, axis=1).dataSync()[0];
-    const labels = ['actinic keratoses and intraepithelial carcinomae(Cancer)', 'basal cell carcinoma(Cancer)', 'benign keratosis-like lesions(Non-Cancerous)', 'dermatofibroma(Non-Cancerous)', 'melanocytic nevi(Non-Cancerous)', 'pyogenic granulomas and hemorrhage(Can lead to cancer)', 'melanoma(Cancer)'];
-    const predictedLabel = labels[predictedClass];
+//     // Get the predicted class and label
+//     const predictedClass = tf.argmax(prediction, axis=1).dataSync()[0];
+//     const labels = ['actinic keratoses and intraepithelial carcinomae(Cancer)', 'basal cell carcinoma(Cancer)', 'benign keratosis-like lesions(Non-Cancerous)', 'dermatofibroma(Non-Cancerous)', 'melanocytic nevi(Non-Cancerous)', 'pyogenic granulomas and hemorrhage(Can lead to cancer)', 'melanoma(Cancer)'];
+//     const predictedLabel = labels[predictedClass];
   
-    // Send the predicted class and label as JSON response
-    res.json({
-      predictedClass,
-      predictedLabel,
-    });
-  });
+//     // Send the predicted class and label as JSON response
+//     res.json({
+//       predictedClass,
+//       predictedLabel,
+//     });
+//   });
 
 
 app.get('/', (req, res)=>{
