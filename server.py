8af32fb75d079fabd6c475a0b237f8635c8edcc0
paulpollob <paulpollob71@gmail.com>
2023-12-04 08:00:09 +0600

    final

--- server.py
+++ server.py
@@ -1,44 +1,126 @@
-import socket
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
-def create_server():
-    # Create a socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to a specific address and port
-    server_address = ('localhost', 8080)
-    # server_address = ('127.0.0.1', 8000)
+app = Flask(__name__)
+CORS(app)
 
-    server_socket.bind(server_address)
+import requests
+from PIL import Image
+from io import BytesIO
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
 
-    # Listen for incoming connections
-    server_socket.listen(1)
-    print(f"Server listening on http://{server_address[0]}:{server_address[1]}/")
 
-    while True:
-        # Wait for a connection
-        print("Waiting for a connection...")
-        client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import os
 
-        # Receive the data from the client
-        request_data = client_socket.recv(1024).decode('utf-8')
-        print(f"Received data:\n{request_data}")
 
-        # Craft a simple HTTP response
-        response_data = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n
-        <html>
-        <head><title>Simple Python Web Server</title></head>
-        <body>
-        <h1>Hello, this is a simple web server!</h1>
-        </body>
-        </html>
-        """
 
-        # Send the response to the client
-        client_socket.sendall(response_data.encode('utf-8'))
+classes = {
+    0: ("actinic keratoses and intraepithelial carcinomae(Cancer)"),
+    1: ("basal cell carcinoma(Cancer)"),
+    2: ("benign keratosis-like lesions(Non-Cancerous)"),
+    3: ("dermatofibroma(Non-Cancerous)"),
+    4: ("melanocytic nevi(Non-Cancerous)"),
+    5: ("pyogenic granulomas and hemorrhage(Can lead to cancer)"),
+    6: ("melanoma(Cancer)"),
+}
+model = load_model('./best_model.h5')
 
-        # Close the connection
-        client_socket.close()
 
-if __name__ == "__main__":
-    create_server()
+
+
+
+
+
+@app.route('/')
+def krishna_route():
+    return 'Hello, Krishna!'
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+
+    print("HK upload\n\n\n\n\n\n\n\n\n")
+    if 'image' in request.files:
+        image_file = request.files['image']
+        print("HK upload2\n\n\n\n\n\n\n\n\n")
+
+
+
+    # Check if the file has an allowed extension
+    if image_file:
+        # Save the file to the root folder of the server
+        file_path = os.path.join('./', "radhe.png")
+        image_file.save(file_path)
+        print("Hk saved\n\n\n\n")
+
+
+
+
+
+
+
+
+    # # Load the 
+    # img = Image.open(image_file)
+    # img = img.convert('RGB')
+
+    # # Resize the image to the target size (28, 28)
+    # img_resized = img.resize((28, 28))
+    # # Convert the resized image to a NumPy array
+    # image = img_to_array(img_resized)
+    # # Normalize the pixel values to be between 0 and 1 (if needed)
+    # image /= 255.0
+
+
+ 
+
+
+    image = load_img('./radhe.png', target_size=(28, 28))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    # Make the prediction
+    prediction = model.predict(image)
+
+    # Get the predicted class and label
+    predicted_class = np.argmax(prediction[0])
+    label = classes[predicted_class]
+
+    # Print the predicted class and label
+    print('Predicted class:', predicted_class)
+    print('Predicted label:', label)
+    # Process or save the image data as needed
+    # For example, you can save it to a file or perform further processing
+
+    # Dummy response for demonstration purposes
+    response_data = {"label":label}
+
+
+    try:
+        # Check if the file exists before attempting to delete
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        else:
+            print(jsonify({'error': f'File not found'}))
+    except Exception as e:
+        print(jsonify({'error': f'Error deleting file: {str(e)}'}))
+
+
+
+
+    
+    return jsonify(response_data)
+
+    # return jsonify({'error': 'No image file provided'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
